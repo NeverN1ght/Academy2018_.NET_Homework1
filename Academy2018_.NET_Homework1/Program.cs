9be06edad38b957f7c812a69c648bb38c453ca02
Mykola Kobets <nicko9298@gmail.com>
2018-07-03 13:30:18 +0300

    Initial commit. Created basic .NET Core Console Application

---
+++ Academy2018_.NET_Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Academy2018_.NET_Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
